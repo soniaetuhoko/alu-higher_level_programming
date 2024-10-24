17f11c797585a150a9d38a5dceb8534d01b348b6
Your Name <your_email@example.com>
2024-10-24 14:49:16 -0700

    add script to update run 1-calculation.py and calculator_1.py

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+result_add = add(a, b)
+result_sub = sub(a, b)
+result_mul = mul(a, b)
+result_div = div(a, b)
+
+print(f"{a} + {b} = {result_add}")
+print(f"{a} - {b} = {result_sub}")
+print(f"{a} * {b} = {result_mul}")
+print(f"{a} / {b} = {result_div}")
+
