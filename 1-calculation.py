7a7d361ec0fca0acf4c60b82479e478a3114d842
Your Name <your_email@example.com>
2024-10-25 08:30:14 -0700

    update all script to run

--- 1-calculation.py
+++ 1-calculation.py
@@ -6,8 +6,7 @@ a = 10
 b = 5
 
 if __name__ == "__main__":
-    print(f"{a} + {b} = {add(a, b)}")
-    print(f"{a} - {b} = {sub(a, b)}")
-    print(f"{a} * {b} = {mul(a, b)}")
-    print(f"{a} / {b} = {div(a, b)}")
-
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
