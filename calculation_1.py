c34d5d4856421b0df97da5a3a238b1063aca2592
Your Name <your_email@example.com>
2024-10-25 03:54:03 -0700

    Set up calculator script and import test

---
+++ calculation_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+if __name__ == "__main__":
+    result_add = add(a, b)
+    result_sub = sub(a, b)
+    result_mul = mul(a, b)
+    result_div = div(a, b)
+
+    print(f"{a} + {b} = {result_add}")
+    print(f"{a} - {b} = {result_sub}")
+    print(f"{a} * {b} = {result_mul}")
+    print(f"{a} / {b} = {result_div}")
+
