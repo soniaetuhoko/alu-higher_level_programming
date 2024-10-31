1da74e17c68588c8365197f3c3340a97e0a38169
Your Name <your_email@example.com>
2024-10-31 03:51:29 -0700

    update script to run all

--- 0-main.py
+++ 0-main.py
@@ -4,11 +4,8 @@ safe_print_list = __import__('0-safe_print_list').safe_print_list
 my_list = [1, 2, 3, 4, 5]
 
 nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))  # Should print: 12\nnb_print: 2
-
+print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))  # Should print: 12345\nnb_print: 5
-
+print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))  # Should print: 12345\nnb_print: 5
-
+print("nb_print: {:d}".format(nb_print))
