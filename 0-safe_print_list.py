d89bb960d5ebd64cbb52cef06b60b6eeca15763f
Your Name <your_email@example.com>
2024-10-31 04:41:24 -0700

    Add safe_print_list function and test

--- 0-safe_print_list.py
+++ 0-safe_print_list.py
@@ -2,10 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        while count < x:
-            print(my_list[count], end="")
+        for i in range(x):
+            print(my_list[i], end='')
             count += 1
     except IndexError:
         pass
-    print()
+    print()  # To ensure a new line after printing
     return count
