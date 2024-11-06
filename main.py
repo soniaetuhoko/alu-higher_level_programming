9d718af0f73f45a618f60f95042cfb6dfb5d688a
Your Name <your_email@example.com>
2024-11-06 12:05:25 -0800

    update all

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-
-from calculation_1 import add, sub, mul, div
-
-a = 10
-b = 5
+from add_0 import add
 
 if __name__ == "__main__":
+    a = 1
+    b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
-
