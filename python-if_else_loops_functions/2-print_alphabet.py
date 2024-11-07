24055b2d3d9a0b8510d1918079935f9f47928bb7
Your Name <your_email@example.com>
2024-11-07 02:28:50 -0800

    python-exceptions

--- python-if_else_loops_functions/2-print_alphabet.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-
-print("{}".format(''.join(chr(i) for i in range(97, 123))), end='')
--- python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-print("{}".format(
-    ''.join(chr(i) for i in range(97, 123) if chr(i) not in ['e', 'q'])
-), end='')
