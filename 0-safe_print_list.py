db4f6e0d628ed8f09e032059c32b2f9158924b7a
Your Name <your_email@example.com>
2024-11-07 07:33:50 -0800

    0-safe_print_list.py

--- 0-safe_print_list.py
+++ 0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    try:
-        while count < x:
-            print(my_list[count], end="")
-            count += 1
-    except IndexError:
-        pass
+    num_returned = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            num_returned += 1
+        except IndexError as IE:
+            break
     print()
-    return count
+    return num_returned
