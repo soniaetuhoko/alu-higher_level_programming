aa11ad08745093714990977ea2433b2ff2946fa4
Your Name <your_email@example.com>
2024-11-08 04:16:52 -0800

    update script to run 2-square.py and 2-main.py

--- python-classes/2-main.py
+++ python-classes/2-main.py
@@ -32,39 +32,3 @@ try:
     print(my_square_4.__dict__)
 except Exception as e:
     print(e)
-
-
-#!/usr/bin/python3
-Square = __import__('3-square').Square
-
-my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
-
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-my_square_2 = Square(5)
-print("Area: {}".format(my_square_2.area()))
-
-
-#!/usr/bin/python3
-Square = __import__('4-square').Square
-
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)
