9f9bd80c7f8714217dfb04d6557eb8fd9282bc82
Your Name <your_email@example.com>
2024-11-09 11:03:55 -0800

    update script to run

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -10,11 +10,14 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates that value is a positive integer."""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+        """Validates value
+        Args:
+            name (str): The name of the parameter. value (int): The value to validate.
+            Raises:
+                TypeError: If value is not an integer. ValueError: If value is less or equal to 0. """ 
+                if not isinstance(value, int): 
+                    raise TypeError(f"{name} must be an integer") if value <= 0: 
+                        raise ValueError(f"{name} must be greater than 0")
 
 
 class Rectangle(BaseGeometry):
