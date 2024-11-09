df48d3b103268708944b51961d4a7e9b5b3f133e
Your Name <your_email@example.com>
2024-11-09 09:48:55 -0800

    update script to run

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+5-base_geometry.py
+
+This module defines an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """An empty class representing base geometry."""
+    pass
