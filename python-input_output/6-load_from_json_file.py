905ca7af3ed0a65aa9dfeebbbcc3abe90c47fb5c
Your Name <your_email@example.com>
2024-11-13 00:46:49 -0800

    update the script to run all

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Module 6-load_from_json_file.py
+Contains a function that creates an object from a JSON file.
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Creates an object from a JSON file.
+
+    Args:
+        filename (str): The name of the file to read from.
+
+    Returns:
+        object: The Python object created from the JSON file.
+    """
+    with open(filename, 'r') as file:
+        return json.load(file)
+
+
+# Test cases
+if __name__ == "__main__":
+    load_json = __import__('6-load_from_json_file').load_from_json_file
+
+    filename = "my_list.json"
+    my_list = load_from_json_file(filename)
+    print(my_list)
+    print(type(my_list))
+
+    filename = "my_dict.json"
+    my_dict = load_from_json_file(filename)
+    print(my_dict)
+    print(type(my_dict))
+
+    try:
+        filename = "my_set_doesnt_exist.json"
+        my_set = load_from_json_file(filename)
+        print(my_set)
+        print(type(my_set))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        filename = "my_fake.json"
+        my_fake = load_from_json_file(filename)
+        print(my_fake)
+        print(type(my_fake))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
