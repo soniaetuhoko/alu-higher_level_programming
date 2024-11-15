fb977c87224ab0acf7766ce5769a234551ed8486
Your Name <your_email@example.com>
2024-11-15 12:49:04 -0800

    update it

---
+++ SQL-introduction/0-list_databases.sql
@@ -0,0 +1,9 @@
+-- This script lists all databases in the MySQL server
+-- Save this script in a file named '0-list_databases.sql'
+
+SHOW DATABASES;
+
+-- To execute the script, use the following command:
+-- guillaume@ubuntu:~/$ cat 0-list_databases.sql | mysql -hlocalhost -uroot -p
+-- Enter password: 
+-- Note: Replace 'localhost' with your MySQL server's hostname and 'root' with your username.
