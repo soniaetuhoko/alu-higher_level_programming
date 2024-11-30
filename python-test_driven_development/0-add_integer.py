b193e86988a9376273c2b829da29648f7477ec8b
Your Name <your_email@example.com>
2024-11-30 06:05:49 -0800

    Addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
+"""Defines an addition function."""
+
 
 def add_integer(a, b=98):
+    """Return the addition of a and b.
+    Float arguments are casted to int before addition is performed.
+    Raises:
+        TypeError: If either a or b is a non-integer and non-float.
     """
-    Adds two integers (or floats) a and b and returns the result as an integer.
-    a and b must be integers or floats, otherwise raises TypeError.
-    """
-    # Check if 'a' is an integer or float, otherwise raise TypeError
-    if not isinstance(a, (int, float)):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    
-    # Check if 'b' is an integer or float, otherwise raise TypeError
-    if not isinstance(b, (int, float)):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-
-    # Cast 'a' and 'b' to integers if they are floats
-    a = int(a)
-    b = int(b)
-
-    # Return the sum of a and b
-    return a + b
-
+    return (int(a) + int(b))
